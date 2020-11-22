87af73ccf7ebd8587a1449b993ded09cf78fdbab
NedWilbur <Ned612@Gmail.com>
2020-11-21 22:27:51 -0500

    Added log level

--- WoWFishBot/Logger.cs
+++ WoWFishBot/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,23 @@ namespace WoWFishBot
 {
     static public class Logger
     {
-        static public void Log(string message, bool showInStatusBar = true)
+        public enum Level { Trace, Debug, Info, Warn, Error }
+
+        static public void Log(string message, Level level, bool showInStatusBar)
         {
             // show in status bar
             if (showInStatusBar) Program.mainForm.UpdateStatusBar(message);
 
             // add timestamp
             DateTime now = DateTime.Now;
-            message = $"[{now.Hour}:{now.Minute}:{now.Millisecond}] {message}";
+            message = $"[{now.Hour}:{now.Minute}:{now.Millisecond}] [{level}] {message}";
 
             // write
             Console.WriteLine(message);
             Program.mainForm.UpdateLog(message);
         }
 
+        static public void Log(string message) => Log(message, Level.Trace, false);
+        static public void StatusBar(string message) => Log(message, Level.Trace, false);
     }
 }
